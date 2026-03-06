72dc10fa5f34653a5880cdf121d70cb08bd21fcf
Steven Brus <steven.r.brus@gmail.com>
2026-03-06 11:41:12 -0800

    Fix issues from rebase

--- components/omega/src/ocn/Tendencies.cpp
+++ components/omega/src/ocn/Tendencies.cpp
@@ -535,7 +535,7 @@ void Tendencies::computeVelocityTendenciesOnly(
 
    Pacer::start("Tend:computeVelocityVAdvTend", 2);
    // Compute velocity tendency from vertical advection
-   VAdv->computeVelocityVAdvTend(NormalVelocityTend, NormalVelEdge,
+   VAdv->computeVelocityVAdvTend(NormalVelocityTend, NormVelEdge,
                                  FluxLayerThickEdge);
    Pacer::stop("Tend:computeVelocityVAdvTend", 2);
 
@@ -702,7 +702,7 @@ void Tendencies::computeTracerTendenciesOnly(
    // compute tracer tendencies from vertical advection
    Array2DReal ThicknessForVAdv;
    if (VAdv->VertAdvChoice == VertAdvOption::Standard) {
-      State->getLayerThickness(ThicknessForVAdv, ThickTimeLevel);
+      ThicknessForVAdv = State->getLayerThickness(ThickTimeLevel);
    } else if (VAdv->VertAdvChoice == VertAdvOption::FCT) {
       ThicknessForVAdv = AuxState->LayerThicknessAux.ProvThickness;
    }

erinethomas/E3SM

change all wave coupling namelists from boolean to char. i.e. wav_ocn_coup = 'none', 'one', or 'two' for no coupling,      one way coupling (ocn to wav), or full two way coupling

components/ww3/src/cpl/wav_comp_mct.F90
@@ -1177,18 +1177,52 @@ CONTAINS
          IX  = MAPSF(ISEA,1)
          IY  = MAPSF(ISEA,2)
          if (MAPSTA(IY,IX) .eq. 1) then
+            if (wav_atm_coup .eq. 'two' .or. wav_ocn_coup .eq. 'two') then
+               w2x_w%rattr(index_w2x_Sw_Charn,jsea) = CHARN(jsea)
+            endif
+            if (wav_ocn_coup .eq. 'two') then
+               w2x_w%rattr(index_w2x_Sw_Hs,jsea) = HS(jsea)
+               w2x_w%rattr(index_w2x_Sw_Fp,jsea) = FP0(jsea)
+               w2x_w%rattr(index_w2x_Sw_Dp,jsea) = THP0(jsea)
+               w2x_w%rattr(index_w2x_Faww_Tawx,jsea) = 1000*TAUWIX(jsea) !Conversion to N m^{-2} by multiplying by density of water (See eqn 2.99 WW3 Manual)
+               w2x_w%rattr(index_w2x_Faww_Tawy,jsea) = 1000*TAUWIY(jsea)
+               w2x_w%rattr(index_w2x_Fwow_Twox,jsea) = 1000*TAUOX(jsea)
+               w2x_w%rattr(index_w2x_Fwow_Twoy,jsea) = 1000*TAUOY(jsea)
+               w2x_w%rattr(index_w2x_Faow_Tocx,jsea) = TAUOCX(jsea)
+               w2x_w%rattr(index_w2x_Faow_Tocy,jsea) = TAUOCY(jsea)
 
-             w2x_w%rattr(index_w2x_Sw_Hs,jsea) = HS(jsea)
-             if (wav_ice_coup) then
-                do ifreq=1,nk 
-                   w2x_w%rattr(index_w2x_Sw_wavespec(ifreq),jsea) = EF(jsea,ifreq)
-                enddo
-             endif
-             if (wav_ocn_coup) then
-                w2x_w%rattr(index_w2x_Sw_Fp,jsea) = FP0(jsea)
-                w2x_w%rattr(index_w2x_Sw_Dp,jsea) = THP0(jsea)
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_1,jsea) = USSP(jsea,1)
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_1,jsea) = USSP(jsea,nk+1)
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_1,jsea) = USSP(jsea,1)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_1,jsea) = USSP(jsea,nk+1)
+
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_2,jsea) = USSP(jsea,2)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_2,jsea) = USSP(jsea,nk+2)
+
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_3,jsea) = USSP(jsea,3)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_3,jsea) = USSP(jsea,nk+3)
+
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_4,jsea) = USSP(jsea,4)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_4,jsea) = USSP(jsea,nk+4)
+
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_5,jsea) = USSP(jsea,5)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_5,jsea) = USSP(jsea,nk+5)
+
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = USSP(jsea,6)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = USSP(jsea,nk+6)
+            endif
+         else
+            if (wav_atm_coup .eq. 'two' .or. wav_ocn_coup .eq. 'two') then
+               w2x_w%rattr(index_w2x_Sw_Charn,jsea) = 0.0
+            endif
+            if (wav_ocn_coup .eq. 'two') then
+               w2x_w%rattr(index_w2x_Sw_Hs,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_Fp,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_Dp,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faww_Tawx,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faww_Tawy,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Fwow_Twox,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Fwow_Twoy,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faow_Tocx,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faow_Tocy,jsea) = 0.0
 
                 w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_2,jsea) = USSP(jsea,2)
                 w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_2,jsea) = USSP(jsea,nk+2)

hyungyukang/Omega

components/ww3/src/cpl/wav_comp_mct.F90
@@ -1186,68 +1186,66 @@ CONTAINS
                w2x_w%rattr(index_w2x_Sw_Charn,jsea) = CHARN(jsea)
             endif
             if (wav_ocn_coup .eq. 'twoway') then
-                w2x_w%rattr(index_w2x_Sw_Hs,jsea) = HS(jsea)
-                w2x_w%rattr(index_w2x_Sw_Fp,jsea) = FP0(jsea)
-                w2x_w%rattr(index_w2x_Sw_Dp,jsea) = THP0(jsea)
+               w2x_w%rattr(index_w2x_Sw_Hs,jsea) = HS(jsea)
+               w2x_w%rattr(index_w2x_Sw_Fp,jsea) = FP0(jsea)
+               w2x_w%rattr(index_w2x_Sw_Dp,jsea) = THP0(jsea)
+               w2x_w%rattr(index_w2x_Faww_Tawx,jsea) = 1000*TAUWIX(jsea) !Conversion to N m^{-2} by multiplying by density of water (See eqn 2.99 WW3 Manual)
+               w2x_w%rattr(index_w2x_Faww_Tawy,jsea) = 1000*TAUWIY(jsea)
+               w2x_w%rattr(index_w2x_Fwow_Twox,jsea) = 1000*TAUOX(jsea)
+               w2x_w%rattr(index_w2x_Fwow_Twoy,jsea) = 1000*TAUOY(jsea)
+               w2x_w%rattr(index_w2x_Faow_Tocx,jsea) = TAUOCX(jsea)
+               w2x_w%rattr(index_w2x_Faow_Tocy,jsea) = TAUOCY(jsea)
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_1,jsea) = USSP(jsea,1)
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_1,jsea) = USSP(jsea,nk+1)
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_1,jsea) = USSP(jsea,1)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_1,jsea) = USSP(jsea,nk+1)
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_2,jsea) = USSP(jsea,2)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_2,jsea) = USSP(jsea,nk+2)
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_2,jsea) = USSP(jsea,2)
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_2,jsea) = USSP(jsea,nk+2)
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_3,jsea) = USSP(jsea,3)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_3,jsea) = USSP(jsea,nk+3)
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_3,jsea) = USSP(jsea,3)
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_3,jsea) = USSP(jsea,nk+3)
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_4,jsea) = USSP(jsea,4)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_4,jsea) = USSP(jsea,nk+4)
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_4,jsea) = USSP(jsea,4)
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_4,jsea) = USSP(jsea,nk+4)
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_5,jsea) = USSP(jsea,5)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_5,jsea) = USSP(jsea,nk+5)
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_5,jsea) = USSP(jsea,5)
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_5,jsea) = USSP(jsea,nk+5)
-
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = USSP(jsea,6)
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = USSP(jsea,nk+6)
-
-                w2x_w%rattr(index_w2x_Faww_Tawx,jsea) = 1000*TAUWIX(jsea) !Conversion to N m^{-2} by multiplying by density of water (See eqn 2.99 WW3 Manual)
-                w2x_w%rattr(index_w2x_Faww_Tawy,jsea) = 1000*TAUWIY(jsea)
-                w2x_w%rattr(index_w2x_Fwow_Twox,jsea) = 1000*TAUOX(jsea)
-                w2x_w%rattr(index_w2x_Fwow_Twoy,jsea) = 1000*TAUOY(jsea)
-                w2x_w%rattr(index_w2x_Faow_Tocx,jsea) = TAUOCX(jsea)
-                w2x_w%rattr(index_w2x_Faow_Tocy,jsea) = TAUOCY(jsea)
-             endif
-          else
-             if (wav_ocn_coup .eq. 'twoway' .or. wav_atm_coup .eq. 'twoway') then
-                w2x_w%rattr(index_w2x_Sw_Charn,jsea) = 0.0
-             endif
-             if (wav_ocn_coup .eq. 'twoway') then
-                w2x_w%rattr(index_w2x_Sw_Hs,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_Fp,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_Dp,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = USSP(jsea,6)
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = USSP(jsea,nk+6)
+            endif
+         else
+            if (wav_ocn_coup .eq. 'twoway' .or. wav_atm_coup .eq. 'twoway') then
+               w2x_w%rattr(index_w2x_Sw_Charn,jsea) = 0.0
+            endif
+            if (wav_ocn_coup .eq. 'twoway') then
+               w2x_w%rattr(index_w2x_Sw_Hs,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_Fp,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_Dp,jsea) = 0.0
             
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_1,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_1,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faww_Tawx,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faww_Tawy,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Fwow_Twox,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Fwow_Twoy,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faow_Tocx,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Faow_Tocy,jsea) = 0.0
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_2,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_2,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_1,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_1,jsea) = 0.0
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_3,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_3,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_2,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_2,jsea) = 0.0
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_4,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_4,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_3,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_3,jsea) = 0.0
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_5,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_5,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_4,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_4,jsea) = 0.0
 
-                w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = 0.0
-             
-                w2x_w%rattr(index_w2x_Faww_Tawx,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Faww_Tawy,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Fwow_Twox,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Fwow_Twoy,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Faow_Tocx,jsea) = 0.0
-                w2x_w%rattr(index_w2x_Faow_Tocy,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_5,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_5,jsea) = 0.0
+
+               w2x_w%rattr(index_w2x_Sw_ustokes_wavenumber_6,jsea) = 0.0
+               w2x_w%rattr(index_w2x_Sw_vstokes_wavenumber_6,jsea) = 0.0
             endif
          endif
       enddo
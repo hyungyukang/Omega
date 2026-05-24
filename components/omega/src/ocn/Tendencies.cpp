00425f5e3b0c9841da177498ac7fdfeec26445df
Hyun Kang <kangh@ornl.gov>
2026-05-24 14:47:25 -0400

    Change the name UnsplitFactor to SplitFactor
    - Changed the name from UnsplitFactor to SplitFactor
    - SSHGrad term is multiplied by SplitFactor now for the barotropic pressure anomaly gradient term.

--- components/omega/src/ocn/Tendencies.cpp
+++ components/omega/src/ocn/Tendencies.cpp
@@ -742,10 +742,10 @@ void Tendencies::computeBaroclinicVelocityTendenciesOnly(
     const AuxiliaryState *AuxState, ///< [in] Auxilary state variables
     int ThickTimeLevel,             ///< [in] Time level
     int VelTimeLevel,               ///< [in] Time level
-    int /*BarotropicVelocityTimeLevel*/, ///< [in] Barotropic velocity time level
-    int BarotropicPressureTimeLevel ///< [in] Barotropic pressure time level
+    int BarotropicVelocityTimeLevel, ///< [in] Barotropic velocity time level
+    int BarotropicPressureTimeLevel, ///< [in] Barotropic pressure time level
+    Real SplitFactor                ///< [in] Split-explicit forcing factor
 ) {
-
    OMEGA_SCOPE(LocNormalVelocityTend, NormalVelocityTend);
    OMEGA_SCOPE(LocPotentialVortHAdv, PotentialVortHAdv);
    OMEGA_SCOPE(LocKEGrad, KEGrad);
@@ -881,7 +881,8 @@ void Tendencies::computeBaroclinicVelocityTendenciesOnly(
              parallelForInner(
                  Team, KRange, INNER_LAMBDA(int KChunk) {
                     LocSSHGrad(LocNormalVelocityTend, IEdge, KChunk,
-                               BtrPressAnomaly, DepthMeanSpecVol);
+                               BtrPressAnomaly, DepthMeanSpecVol,
+                               SplitFactor);
                  });
           });
       Pacer::stop("Tend:BclBtrPressureGrad", 2);
@@ -899,6 +900,7 @@ void Tendencies::computeBaroclinicVelocityTendencies(
     int VelTimeLevel,               ///< [in] Time level
     int BarotropicVelocityTimeLevel, ///< [in] Barotropic velocity time level
     int BarotropicPressureTimeLevel, ///< [in] Barotropic pressure time level
+    Real SplitFactor,               ///< [in] Split-explicit forcing factor
     TimeInterval ProjDt ///< [in] Time interval for projection over the current
                         ///< time stepper stage
 ) {
@@ -910,7 +912,8 @@ void Tendencies::computeBaroclinicVelocityTendencies(
    computeBaroclinicVelocityTendenciesOnly(State, AuxState, ThickTimeLevel,
                                            VelTimeLevel,
                                            BarotropicVelocityTimeLevel,
-                                           BarotropicPressureTimeLevel);
+                                           BarotropicPressureTimeLevel,
+                                           SplitFactor);
 
    Pacer::stop("Tend:computeBaroclinicVelocityTendencies", 1);
 }

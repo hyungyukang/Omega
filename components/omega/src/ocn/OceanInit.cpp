00a67b4725e78660125e83e4eb696bd7815d168b
Hyun Kang <kangh@ornl.gov>
2026-05-25 11:46:37 -0400

    Fix bugs and some updates

--- components/omega/src/ocn/OceanInit.cpp
+++ components/omega/src/ocn/OceanInit.cpp
@@ -177,7 +177,8 @@ int ocnInit(MPI_Comm Comm ///< [in] ocean MPI communicator
    }
 
    // If reading from restart, reset the current time to the input time
-   if (SimTimeStr != " ") {
+   const bool ReadRestart = SimTimeStr != " ";
+   if (ReadRestart) {
       TimeInstant NewCurrentTime(SimTimeStr);
       ModelClock->setCurrentTime(NewCurrentTime);
    }
@@ -186,7 +187,7 @@ int ocnInit(MPI_Comm Comm ///< [in] ocean MPI communicator
    // fields
 
    OceanState *DefState = OceanState::getDefault();
-   DefStepper->initializeStateFromInput(DefState, !Err2.isFail());
+   DefStepper->initializeStateFromInput(DefState, ReadRestart);
 
    I4 CurTimeLevel      = 0;
    DefState->exchangeHalo(CurTimeLevel);

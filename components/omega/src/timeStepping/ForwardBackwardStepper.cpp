6ce4598af271a787216ef36d3f5459044ae6f582
Andrew Nolan <anolan@lanl.gov>
2026-07-31 16:47:35 -0400

    Add missing halo exchanges

--- components/omega/src/timeStepping/ForwardBackwardStepper.cpp
+++ components/omega/src/timeStepping/ForwardBackwardStepper.cpp
@@ -40,6 +40,10 @@ void ForwardBackwardStepper::doStep(
    Array3DReal CurTracerArray  = Tracers::getAll(TracerCurLevel);
    Array3DReal NextTracerArray = Tracers::getAll(TracerNextLevel);
 
+   VertMix *VMix = VertMix::getInstance();
+
+   const MPI_Comm Comm = MeshHalo->getComm();
+
    if (State == nullptr)
       LOG_CRITICAL("Invalid State");
    if (AuxState == nullptr)
@@ -57,10 +61,15 @@ void ForwardBackwardStepper::doStep(
    // u^{n+1} = u^{n} + R_u^{n}
    updateVelocityByTend(State, VelNextLevel, State, VelCurLevel, TimeStep);
 
-   updateVelocityByTend(State, VelNextLevel, State, VelCurLevel, TimeStep);
-
    prescribeVelocity(State, VelNextLevel, State, VelCurLevel,
                      SimTime + TimeStep);
+
+   // Exchange halo of u^{n+1} before it is used below
+   Array2DReal NextNormalVelocity = State->getNormalVelocity(VelNextLevel);
+   Pacer::timingBarrier("ForwardBackward:velHaloExchBarrier", 3, Comm);
+   Pacer::start("ForwardBackward:velHaloExch", 3);
+   MeshHalo->exchangeFullArrayHalo(NextNormalVelocity, OnEdge);
+   Pacer::stop("ForwardBackward:velHaloExch", 3);
 
    // R_h^{n} = RHS_h(u^{n+1}, h^{n}, t^{n})
    Tend->computePseudoThicknessTendencies(State, AuxState, ThickCurLevel,
@@ -70,6 +79,13 @@ void ForwardBackwardStepper::doStep(
    updateThicknessByTend(State, ThickNextLevel, State, ThickCurLevel, TimeStep);
 
    prescribeThickness(State, ThickNextLevel, State, ThickCurLevel);
+
+   // Exchange halo of h^{n+1} before it is used below
+   Array2DReal NextPseudoThickness = State->getPseudoThickness(ThickNextLevel);
+   Pacer::timingBarrier("ForwardBackward:thickHaloExchBarrier", 3, Comm);
+   Pacer::start("ForwardBackward:thickHaloExch", 3);
+   MeshHalo->exchangeFullArrayHalo(NextPseudoThickness, OnCell);
+   Pacer::stop("ForwardBackward:thickHaloExch", 3);
 
    // R_phi^{n} = RHS_phi(u^{n+1}, h^{n+1}, phi^{n}, t^{n})
    Tend->computeTracerTendencies(State, AuxState, CurTracerArray,
@@ -81,7 +97,6 @@ void ForwardBackwardStepper::doStep(
 
    // Update time levels (New -> Old) of prognostic variables with halo
    // exchanges
-   const MPI_Comm Comm = MeshHalo->getComm();
    Pacer::timingBarrier("ForwardBackward:haloExchBarrier", 3, Comm);
    Pacer::start("ForwardBackward:haloExch", 3);
    State->updateTimeLevels();

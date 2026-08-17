e4da09df2844318c86685ee6c9ddf9ea1d1f0f77
Alice Barthel <ambarthel26@gmail.com>
2026-08-17 09:52:49 -0700

    added missing arguments

--- components/omega/src/ocn/Tendencies.cpp
+++ components/omega/src/ocn/Tendencies.cpp
@@ -942,8 +942,10 @@ void Tendencies::computeTracerTendenciesOnly(
           ForcingState->TracerForcing.SeaIceHeatFluxCell;
       const auto &ShortWaveHeatFlux =
           ForcingState->TracerForcing.ShortWaveHeatFluxCell;
-      const auto &SnowFlux      = ForcingState->TracerForcing.SnowFluxCell;
-      const auto &RainFlux      = ForcingState->TracerForcing.RainFluxCell;
+      const auto &SnowFlux = ForcingState->TracerForcing.SnowFluxCell;
+      const auto &RainFlux = ForcingState->TracerForcing.RainFluxCell;
+      const auto &EvaporationFlux =
+          ForcingState->TracerForcing.EvaporationFluxCell;
       const auto &IceRunoffFlux = ForcingState->TracerForcing.IceRunoffFluxCell;
       const auto &RiverRunoffFlux =
           ForcingState->TracerForcing.RiverRunoffFluxCell;
@@ -958,7 +960,7 @@ void Tendencies::computeTracerTendenciesOnly(
                                  LongWaveHeatFluxUp, LongWaveHeatFluxDown,
                                  SeaIceHeatFlux, ShortWaveHeatFlux, SnowFlux,
                                  RainFlux, IceRunoffFlux, RiverRunoffFlux,
-                                 SeaIceSaltFlux);
+                                 EvaporationFlux, SeaIceSaltFlux);
           });
       Pacer::stop("Tend:sfcTracerForcing", 2);
    }
